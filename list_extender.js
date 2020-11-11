6153a70cecdafee98a33eda6e9511b836ee95ff3
JLambertazzo <42924425+JLambertazzo@users.noreply.github.com>
2020-11-11 13:57:38 -0600

    added ability to add and remove inputs fields, and add list items

--- list_extender.js
+++ list_extender.js
@@ -2,13 +2,36 @@
 console.log('----------')
 console.log('list extender loaded')
 
-function exlUnordered (classes = '', id = null) {
+function ExtendingList (classes = '', id = null) {
   const _self = document.createElement('UL')
   classes.split(' ').forEach(c => {
     _self.classList.add(c)
   })
   if (id) {
     _self.id = id
+  }
+
+  _self.listItems = []
+  _self.inputFields = []
+
+  _self.addInputField = (type = 'text', placeholder = '') => {
+    const newInput = document.createElement('INPUT')
+    newInput.setAttribute('type', type)
+    newInput.setAttribute('placeholder', placeholder)
+    _self.inputFields.push(newInput)
+  }
+
+  _self.removeInputField = fieldNum => {
+    _self.inputFields = _self.inputFields.slice(fieldNum - 1, fieldNum)
+  }
+
+  _self.addListItem = () => {
+    const li = document.createElement('LI')
+    for (let i = 0; i < _self.inputFields.length; i++) {
+      li.appendChild(_self.inputFields[i])
+    }
+    li.setAttribute('key', _self.listItems.length)
+    _self.appendChild(li)
   }
 
   return _self
--- test.js
+++ test.js
@@ -2,6 +2,11 @@
 
 const log = console.log
 
-const myUL = exlUnordered('class1 class2', 'myID')
+const myUL = ExtendingList('class1 class2', 'myID')
 log('UL - Given classes: ' + myUL.classList)
 log('UL - Given id: ' + myUL.id)
+document.body.appendChild(myUL)
+myUL.addInputField()
+myUL.addInputField('password')
+myUL.addInputField('', 'THIRD INPUT WOHOO')
+myUL.addListItem()
