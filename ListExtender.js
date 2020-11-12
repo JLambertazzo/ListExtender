51d30ae13d4a54bc66863cfd16b957eaf6b098a1
JLambertazzo <42924425+JLambertazzo@users.noreply.github.com>
2020-11-12 14:45:01 -0600

    Implemented input to list and list to input with validation
    - Turn inputs to li when focus lost (if valid input)
    - Turn li to inputs when clicked
    - Added event listener to add more input elements at end of list

--- ListExtender.js
+++ ListExtender.js
@@ -21,18 +21,35 @@ function ExtendingList (classes = '', id = null) {
     input.setAttribute('type', _self.inputType)
     input.setAttribute('placeholder', _self.inputPlaceholder)
     input.setAttribute('key', _self.listItems.length)
+    input.setAttribute('required', '')
     return input
   }
 
-  const validate = () => {
+  const validate = input => {
     // Validates the current active input
+    if (input.checkValidity()) {
+      return true
+    } else {
+      input.reportValidity()
+      return false
+    }
   }
 
-  const turnToList = () => {
+  const turnToList = input => {
     // Turns the active input to a list element
+    const li = input.parentElement
+    li.removeChild(input)
+    li.appendChild(document.createTextNode(input.value))
+  }
+
+  const turnToInput = li => {
+    const input = getInputElement()
+    input.value = li.innerText
+    li.removeChild(li.firstChild)
+    li.appendChild(input)
   }
   /* ========================= */
-  
+
   _self.setInputType = type => {
     _self.inputType = type
   }
@@ -48,12 +65,33 @@ function ExtendingList (classes = '', id = null) {
     _self.appendChild(li)
   }
 
-  _self.addEventListener('mousedown', event => {
-    // If target is not input, force focus out event
+  _self.addEventListener('focusout', event => {
+    // Validate, and turn to list
+    if (validate(event.target)) {
+      turnToList(event.target)
+    } else {
+      event.preventDefault()
+    }
   })
 
-  _self.addEventListener('focusout', () => {
-    // Validate, and turn to list
+  _self.addEventListener('mousedown', event => {
+    console.log(event.target)
+    if (event.target.tagName === 'LI') {
+      turnToInput(event.target)
+    }
+  })
+
+  _self.addEventListener('input', event => {
+    // if all inputs are valid, add another to list
+    // TODO don't check newly added element
+    const inputs = _self.querySelectorAll('input')
+    for (let i = 0; i < inputs.length; i++) {
+      if (!inputs[i].checkValidity()) {
+        return
+      }
+    }
+
+    _self.addListItem()
   })
 
   return _self
