e7963987be6f9dd62129b76c7b0616b3906b80f9
Julien <jbertazzolambert@gmail.com>
2020-12-16 23:24:57 -0600

    Set up paths for new page

--- app.js
+++ app.js
@@ -6,7 +6,15 @@ const app = express()
 app.use(express.static(path.join(__dirname, '/pub')))
 
 app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'pub/index.html'))
+})
+
+app.get('/examples', (req, res) => {
   res.sendFile(path.join(__dirname, 'pub/examples.html'))
+})
+
+app.get('/documentation', (req, res) => {
+  res.sendFile(path.join(__dirname, 'pub/documentation.html'))
 })
 
 const port = process.env.PORT || 5000
